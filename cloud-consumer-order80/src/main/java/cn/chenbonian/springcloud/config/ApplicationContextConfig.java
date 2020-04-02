15f4274ac459247fc36e72fe5776d5b60161675c
chbn <284049379@qq.com>
2020-04-03 00:26:46 +0800

    项目重构

---
+++ cloud-consumer-order80/src/main/java/cn/chenbonian/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,36 @@
+package cn.chenbonian.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 相当于一个Spring的xml的配置文件，用IOC的方式创建一个新对象
+ * @author chbn
+ * @create 2020-04-02 21:46
+ */
+
+/*
+    SpringBoot优化了spring的配置文件
+    将spring的 applicationContext.xml 简化成注解版的@Configuration配置
+    加了@Configuration改注解的类等同于以前Spring的applicationContext.xml文件
+    原来applicationContext.xml文件中要加的 <bean id="userService"   class="com.atguigu.tmall.UserServiceImpl"/>
+    变成
+    @Bean
+    public UserService getUserService(){
+        return new UserServiceImpl();
+    }
+
+    其中@Bean相当于原来 xml文件中的 <bean/> 节点
+    id 相当于返回值方法返回值类型  class相当于返回值
+
+ */
+@Configuration
+public class ApplicationContextConfig {
+    // 相当于Spring的 applicationContextConfig.xml的配置文件，<bean id="" class="">
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+}
--- cloud-provider-payment8001/src/main/java/cn/chenbonian/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/cn/chenbonian/springcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import cn.chenbonian.springcloud.entities.CommonResult;
 import cn.chenbonian.springcloud.entities.Payment;
 import cn.chenbonian.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -24,9 +21,9 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
-        log.info("插入结果："+result);
+        log.info("插入数据返回值："+result);
         if(result>0){
             return new CommonResult(200,"插入数据成功",result);
         }else {
@@ -37,7 +34,7 @@ public class PaymentController {
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id){
         Payment payment = paymentService.getPaymentById(id);
-        log.info("插入结果："+payment);
+        log.info("查询id："+id);
         if(payment != null){
             return new CommonResult(200,"查询成功",payment);
         }else {
