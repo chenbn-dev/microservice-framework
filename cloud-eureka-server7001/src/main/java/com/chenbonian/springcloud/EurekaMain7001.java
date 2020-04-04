990f8689b7c3b9e843456c4060b93adab53c7f49
chbn <284049379@qq.com>
2020-04-04 11:21:21 +0800

    添加EurekaServer和EurekaClient并测试通过

---
+++ cloud-eureka-server7001/src/main/java/com/chenbonian/springcloud/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package com.chenbonian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka服务端的主启动类
+ * @author chbn
+ * @create 2020-04-03 1:06
+     */
+
+@SpringBootApplication
+@EnableEurekaServer // 表示是eureka的服务注册中心
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
--- cloud-provider-payment8001/src/main/java/cn/chenbonian/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/cn/chenbonian/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package cn.chenbonian.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 支付模块主启动类
@@ -10,7 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-    public class PaymentMain8001 {
+@EnableEurekaClient
+public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
     }
