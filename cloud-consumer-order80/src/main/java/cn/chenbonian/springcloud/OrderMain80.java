8ce17f408d687ea73a888d35408f837227b62b23
chbn <284049379@qq.com>
2020-04-04 11:34:12 +0800

    将cloud-consumer-order80注册进EurekaSrever

--- cloud-consumer-order80/src/main/java/cn/chenbonian/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/cn/chenbonian/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package cn.chenbonian.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 消费者主启动类， 80是http协议默认端口，可以省略，对于用户更友好
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create 2020-04-02 0:47
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
