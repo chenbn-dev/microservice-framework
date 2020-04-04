0671403e6a94d5538e470043d29ed7546c4bfb91
chbn <284049379@qq.com>
2020-04-05 01:24:16 +0800

    Eureka服务发现

--- cloud-provider-payment8001/src/main/java/cn/chenbonian/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/cn/chenbonian/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import cn.chenbonian.springcloud.entities.Payment;
 import cn.chenbonian.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 控制层
@@ -23,6 +26,9 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
@@ -45,4 +51,20 @@ public class PaymentController {
             return new CommonResult(444,"没有对应记录，查询ID："+id ,null);
         }
     }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery(){
+        // 获得在Eureka中注册的所有微服务，即微服务清单列表
+        List<String> services = discoveryClient.getServices();
+        for (String service : services){
+            log.info("Eureka集群中注册的微服务:" + service);
+        }
+        // 根据微服务的具体服务名称，进一步获得这个微服务的相关信息（一个微服务下的全部实例）
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance instance : instances){
+            log.info("serviceID："+instance.getServiceId()+"；\t"+"Host："+instance.getHost()+"；\t"+"Port："+instance.getPort()+"；\t"+"Uri:"+instance.getUri());
+        }
+        return this.discoveryClient;
+    }
+
 }
