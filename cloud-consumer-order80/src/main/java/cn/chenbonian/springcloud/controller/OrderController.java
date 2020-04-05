fec28817c22c3a9ec7051522ee366c83c8ed1f9a
chbn <284049379@qq.com>
2020-04-05 17:28:15 +0800

    restTemplate方式第二种方式的调用

--- cloud-consumer-order80/src/main/java/cn/chenbonian/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/cn/chenbonian/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package cn.chenbonian.springcloud.controller;
 import cn.chenbonian.springcloud.entities.CommonResult;
 import cn.chenbonian.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +36,17 @@ public class OrderController  {
         log.info("请求id为"+id+"的数据");
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id")Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+        if(entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommonResult(444,"操作失败 ");
+        }
+
+    }
+
 
 }
