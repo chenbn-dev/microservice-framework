51b69eed3cccf82f86a1c01ce2547bededbd7a17
chbn <284049379@qq.com>
2020-04-05 19:25:48 +0800

    替换Ribbon负载均衡规则

---
+++ cloud-consumer-order80/src/main/java/cn/chenbonian/myrule/MySelfRule.java
@@ -0,0 +1,22 @@
+package cn.chenbonian.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 自定义负载均衡路由规则类
+ * @author chbn
+ * @create 2020-04-05 17:47
+ */
+
+@Configuration
+public class MySelfRule {
+    @Bean
+    public IRule myRule() {
+        // 定义为随机
+        return new RandomRule();
+    }
+}
+ 
--- cloud-consumer-order80/src/main/java/cn/chenbonian/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/cn/chenbonian/springcloud/OrderMain80.java
@@ -1,8 +1,10 @@
 package cn.chenbonian.springcloud;
 
+import cn.chenbonian.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * 向Eureka中注册的消费者主启动类， 80是http协议默认端口，可以省略，对于用户更友好
@@ -11,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
