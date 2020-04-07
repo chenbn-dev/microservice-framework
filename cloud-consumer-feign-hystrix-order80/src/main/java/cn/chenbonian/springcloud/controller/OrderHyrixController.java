679ab0d6df87d40e26825739320b7dda301d6093
chbn <284049379@qq.com>
2020-04-07 23:37:25 +0800

    解决服务降价的方法重复导致的代码冗余问题

--- cloud-consumer-feign-hystrix-order80/src/main/java/cn/chenbonian/springcloud/controller/OrderHyrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/cn/chenbonian/springcloud/controller/OrderHyrixController.java
@@ -1,8 +1,8 @@
 package cn.chenbonian.springcloud.controller;
 
 import cn.chenbonian.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +16,7 @@ import javax.annotation.Resource;
  */
 @Slf4j
 @RestController
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderHyrixController {
 
     @Resource
@@ -29,9 +30,10 @@ public class OrderHyrixController {
 
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+//    })
+    @HystrixCommand
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         //int age = 10/0;
         return paymentHystrixService.paymentInfo_TimeOut(id);
@@ -40,5 +42,12 @@ public class OrderHyrixController {
     public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
         return "我是消费者80,对方支付系统繁忙请10秒种后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
     }
-
+    /**
+     * 全局fallback
+     *
+     * @return
+     */
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息,请稍后重试";
+    }
 }
