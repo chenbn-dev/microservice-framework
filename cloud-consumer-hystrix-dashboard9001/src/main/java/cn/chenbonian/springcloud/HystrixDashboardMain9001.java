2dd0d756bdefb7138d98ce30935909078851ff2f
chbn <284049379@qq.com>
2020-04-08 23:53:56 +0800

    服务监控hystrixDashboard仪表盘配置。

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/cn/chenbonian/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,23 @@
+package cn.chenbonian.springcloud;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author chbn
+ * @create 2020-04-08 22:53
+ */
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class);
+    }
+
+
+}
