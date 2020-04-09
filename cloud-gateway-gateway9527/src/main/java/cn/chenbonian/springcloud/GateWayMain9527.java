a7c43979da8c0de12c7fc885c95323099fcb2708
chbn <284049379@qq.com>
2020-04-09 22:44:37 +0800

    gateway路由映射配置及动态路由配置。

---
+++ cloud-gateway-gateway9527/src/main/java/cn/chenbonian/springcloud/GateWayMain9527.java
@@ -0,0 +1,17 @@
+package cn.chenbonian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author chbn
+ * @create 2020-04-09 0:17
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
